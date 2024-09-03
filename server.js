143ad49c1e274d4a0ffa21bd46b2babda558b663
MachariaP <walburphinehas78@gmail.com>
2024-09-03 11:55:24 +0300

    First API

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import routes from './routes/index.js';
+
+// Load environment variables from a .env file
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Load all routes
+app.use('/', routes);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
