2e28379256795c602a31836882564ba6d8de6bbc
MachariaP <walburphinehas78@gmail.com>
2024-09-04 10:37:45 +0300

    Resolved merge conflicts and updated server configuration

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,11 @@
 
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 /**
- * Route to get the status of the application.
+ * Route to get the statistics of the application.
  * @name GET /status
  * @param {Object} req - Express request object.
  * @param {Object} res - Express response object.
@@ -23,13 +22,5 @@ router.get('/status', async (req, res) => AppController.getStatus(req, res));
  * @param {Object} res - Express response object.
  */
 router.get('/stats', async (req, res) => AppController.getStats(req, res));
-
-/**
- * Route to create a new user.
- * @name POST /users
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
-router.post('/users', async (req, res) => UsersController.postNew(req, res));
 
 export default router;
